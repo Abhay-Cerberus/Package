8126a2757c7ce935fcf29348ac5cabefeb9f9c4c
Mister Cerberus <94872391+Abhay-Cerberus@users.noreply.github.com>
2025-04-06 00:31:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="package",
+    name="InternshipPackage",
     version="0.1.0",
     packages=find_packages(include=["modules", "modules.*"]),
     description="A package for OCR, XAI, data augmentation, and sentient analyzer",
