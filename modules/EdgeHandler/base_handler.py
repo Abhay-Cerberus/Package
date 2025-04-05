f11640428f9d88efe86c096143b18b181920d343
Mister Cerberus <94872391+Abhay-Cerberus@users.noreply.github.com>
2025-04-05 22:33:49 +0530

    Delete modules/EdgeHandler directory

--- modules/EdgeHandler/base_handler.py
+++
@@ -1,9 +0,0 @@
-class BaseModelHandler:
-    def optimize(self, techniques):
-        raise NotImplementedError
-
-    def convert(self, target_format):
-        raise NotImplementedError
-
-    def evaluate(self, test_data):
-        raise NotImplementedError
