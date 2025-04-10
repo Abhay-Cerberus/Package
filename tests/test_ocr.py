6520862cbdf3456e361f0e00902a591b381ebeeb
Abhay-Cerberus <94872391+Abhay-Cerberus@users.noreply.github.com>
2025-04-10 21:30:19 +0530

    updated tests

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,5 +1,12 @@
 from AiPackageWrapper.ocr_module import OCRProcessor
 
-def test_extract():
-    result  = OCRProcessor.extract_text("./Sample_photo.png")
-    result == "Hello Interns"
+from unittest.mock import patch, MagicMock
+import pytest
+
+@patch("modules.ocr_module.Image.open")
+@patch("modules.ocr_module.tr_ocr_model")
+def test_extract(mock_model, mock_image):
+    mock_model.return_value = "Detected text"
+    mock_image.return_value = MagicMock()
+    text = OCRProcessor.extract_text("fake_path.png")
+    assert text == "Detected text"
